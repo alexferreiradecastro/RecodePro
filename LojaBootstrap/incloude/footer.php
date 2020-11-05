9e03578d88b2cbf5eac21ef46ec384fab6b5ebc1
alexferreiradecastro <alexferreiradecastro@hotmail.com>
2020-11-05 00:30:07 -0300

    Bootstrap e refatoração

---
+++ LojaBootstrap/incloude/footer.php
@@ -0,0 +1,7 @@
+<div>
+    <footer id="rodape">
+        <p id="formas_pagamento"><b>Formas de Pagamento</b></p>
+        <img width="378" height="55" src="./imagens/formasPagamento.png" alt="Formas de pagamento">
+        <p>&copy; Recode Pro</p>
+    </footer>
+</div>
---
+++ LojaBootstrap/incloude/menu.php
@@ -0,0 +1,9 @@
+
+<div>
+    <nav  class="navbar"  style="background-color:#DC2D36 ">
+        <a  href="index.php"><img width="78px" src="./imagens/fullstackEletro.png" alt="Full Stack Eletro"></a>
+        <a class="cor-header" href="produtos.php" > Produtos</a>
+        <a class="cor-header" href="loja.php"> Nossas lojas</a>
+        <a class="cor-header" href="contato.php"> Fale Conosco</a>
+    </nav>
+</div>
