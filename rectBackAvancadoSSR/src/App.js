2aadaa0c3ed29052f09c88941f5047a2870417ec
alexferreiradecastro <alexferreiradecastro@hotmail.com>
2021-02-10 18:01:34 -0300

    Side Server Rendering

---
+++ rectBackAvancadoSSR/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './Home'
+
+//A página home também funciona com SSR - Side Server Rendering
+function App() {
+    return (
+      
+      <Home/>
+    );
+}
+
+export default App;
